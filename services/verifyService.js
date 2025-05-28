50fb2bce381042341054c5b86a4dd08da4a84a0c
arirang030 <arirang030@kookmin.ac.kr>
2025-05-28 15:02:25 +0900

    [FIX] 코드 수정

--- services/verifyService.js
+++ services/verifyService.js
@@ -38,6 +38,20 @@ class VerifyService {
 
     return { message };
   }
-}
+
+    // DB에서 인증 자료 리스트 가져오기
+  async getFiles() {
+    return await repo.findFiles();
+  };
+
+  // 인증 자료 확인 여부 체크
+  async checkVerified(userId) {
+    const file = await repo.findByUserId(userId);
+    if (!file) {
+      throw new VerificationNotFoundError();
+    }
+    return file.isChecked;
+  };
+};
 
 module.exports = new VerifyService();
